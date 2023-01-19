f118fcfbb91b376721805432f59a988c24604579
Javi Moreno <email@javier-moreno.com>
2023-01-19 15:28:12 +0000

    Moved permissions outside of main

--- 00-network-landing-zone/main.tf
+++ 00-network-landing-zone/main.tf
@@ -22,31 +22,3 @@ data "aws_ami" "app1_ubuntu" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 }
-
-resource "aws_iam_instance_profile" "dev-resources-iam-profile" {
-  name = "ec2_profile"
-  role = aws_iam_role.dev-resources-iam-role.name
-}
-
-resource "aws_iam_role" "dev-resources-iam-role" {
-  name               = "dev-ssm-role"
-  description        = "The role for the developer resources EC2"
-  assume_role_policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "ec2.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    }
-  EOF
-}
-
-resource "aws_iam_role_policy_attachment" "dev-resources-ssm-policy" {
-  role       = aws_iam_role.dev-resources-iam-role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
