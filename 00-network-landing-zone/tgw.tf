2c68fd4df841d125e8c885f217123914508f36a7
Javi Moreno <email@javier-moreno.com>
2023-01-19 15:26:11 +0000

    Fixed file names

--- 00-network-landing-zone/tgw.tf
+++ 00-network-landing-zone/tgw.tf
@@ -1,52 +1,43 @@
-provider "aws" {
-  region = local.region
-}
+## Add tgw route 0.0.0.0/0 -> ingress vpc
 
-locals {
-  name   = "demo-tgw-${replace(basename(path.cwd), "_", "-")}"
-  region = "eu-central-1"
+module "tgw" {
+  source  = "terraform-aws-modules/transit-gateway/aws"
+  version = "2.8.1"
 
-  tags = {
-    Example  = local.name
-    Owner    = "jmoreno"
-    Creation = timestamp()
+  name            = local.name
+  description     = "My TGW shared with several other AWS accounts"
+  amazon_side_asn = 64532
+
+  transit_gateway_cidr_blocks = ["10.99.0.0/24"]
+
+  # When "true" there is no need for RAM resources if using multiple AWS accounts
+  enable_auto_accept_shared_attachments = true
+
+  # When "true", allows service discovery through IGMP
+  enable_mutlicast_support = false
+
+  share_tgw = false
+
+  vpc_attachments = {
+    vpc_ingress = {
+      vpc_id       = module.vpc_ingress.vpc_id
+      subnet_ids   = module.vpc_ingress.private_subnets
+      dns_support  = true
+      ipv6_support = false
+      tags = merge(local.tags, {
+        Name : "${local.name}-tgw-attachment-ingress"
+      })
+    },
+    vpc_inner = {
+      vpc_id       = module.vpc_inner.vpc_id
+      subnet_ids   = module.vpc_inner.private_subnets
+      dns_support  = true
+      ipv6_support = false
+      tags = merge(local.tags, {
+        Name : "${local.name}-tgw-attachment-inner"
+      })
+    },
   }
+
+  tags = local.tags
 }
-
-data "aws_ami" "app1_ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] /* Ubuntu */
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-}
-
-resource "aws_iam_instance_profile" "dev-resources-iam-profile" {
-  name = "ec2_profile"
-  role = aws_iam_role.dev-resources-iam-role.name
-}
-
-resource "aws_iam_role" "dev-resources-iam-role" {
-  name               = "dev-ssm-role"
-  description        = "The role for the developer resources EC2"
-  assume_role_policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "ec2.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    }
-  EOF
-}
-
-resource "aws_iam_role_policy_attachment" "dev-resources-ssm-policy" {
-  role       = aws_iam_role.dev-resources-iam-role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
